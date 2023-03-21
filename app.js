b752e77b3129b04eb6f4a90001e1bf72a4663e23
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-21 11:18:49 +0300

    mongoose setup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,38 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+main().catch((err) => console.log(err));
+
+async function main() {
+	const url = "mongodb://127.0.0.1:27017/userDB";
+	await mongoose.connect(url, { useNewUrlParser: true });
+}
+
+const userSchema = new mongoose.Schema({
+	email: String,
+	password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+app.get("/register", (req, res) => {
+	res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    
+})
 
 app.listen(PORT, () => {
 	console.log(`Server started at port ${PORT}`);
