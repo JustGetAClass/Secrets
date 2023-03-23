aeae32f625f0dc4d525d1909e5c80639cd7d4640
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-23 11:55:06 +0300

    encrypting password

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 const PORT = 3000;
@@ -20,6 +21,9 @@ const userSchema = new mongoose.Schema({
 	email: String,
 	password: String,
 });
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
